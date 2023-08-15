2c40f3180b777430f57a7fc7a7393dcf66052d3b
frankqianms <frankqian@microsoft.com>
2023-08-15 17:24:15 +0800

    chore: changes

--- bots/utils/agent.py
+++ bots/utils/agent.py
@@ -14,43 +14,6 @@ os.environ["OPENAI_API_VERSION"]=os.environ.get("OPENAI_CHAT_API_VERSION")
 os.environ["OPENAI_API_BASE"]=os.environ.get("OPENAI_API_BASE")
 os.environ["OPENAI_API_KEY"]=os.environ.get("OPENAI_API_KEY")
 llm = AzureChatOpenAI(deployment_name='gpt35', model_name='gpt-35-turbo', temperature=0.9)
-    
-class FakeAgent(BaseSingleActionAgent):
-    """Fake Custom Agent."""
-    
-    @property
-    def input_keys(self):
-        return ["input"]
-    
-    def plan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[AgentAction, AgentFinish]:
-        """Given input, decided what to do.
- 
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
- 
-        Returns:
-            Action specifying what tool to use.
-        """
-        return AgentAction(tool="Search", tool_input=kwargs["input"], log="")
- 
-    async def aplan(
-        self, intermediate_steps: List[Tuple[AgentAction, str]], **kwargs: Any
-    ) -> Union[AgentAction, AgentFinish]:
-        """Given input, decided what to do.
- 
-        Args:
-            intermediate_steps: Steps the LLM has taken to date,
-                along with observations
-            **kwargs: User inputs.
- 
-        Returns:
-            Action specifying what tool to use.
-        """
-        return AgentAction(tool="Search", tool_input=kwargs["input"], log="")
 
 # load tools
 tools = load_tools(["serpapi", "llm-math"], llm=llm)
@@ -65,5 +28,3 @@ agent = initialize_agent(
     handle_parsing_errors=True,
     handle_llm_errors=True,
 )
-# agent = FakeAgent()
-# agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
