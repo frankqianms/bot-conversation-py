7b7c2ef39ad00bb419b28a49bb70e03b5bf326cc
frankqianms <frankqian@microsoft.com>
2023-08-31 16:23:04 +0800

    init

--- bots/askAI.py
+++ bots/askAI.py
@@ -7,6 +7,8 @@ from bots.utils.retriever import create_vectorstore
 
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.agents.tools import Tool
+from langchain.agents import load_tools, initialize_agent, AgentType
 
 class AIAsker:
     def __init__(self, loader=None):
@@ -16,6 +18,7 @@ class AIAsker:
         self._memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
         self._docs = None
         self._vectorstore = None
+        self._agent = None
 
     async def askAI(self, query: str, loader=None):
         # question prompt
@@ -28,10 +31,24 @@ class AIAsker:
         if self._docs is None:
             docs = await loader.split()
             self._vectorstore = await create_vectorstore(docs)
-        qa = ConversationalRetrievalChain.from_llm(llm, self._vectorstore.as_retriever(), memory=self._memory)
-        self._chat_history = [(query, self._answer["answer"] if self._answer else "")]
-        self._answer = qa({"question": query, "chat_history": self._chat_history})
+
+        if self._agent is None:
+            qa = ConversationalRetrievalChain.from_llm(llm, self._vectorstore.as_retriever(), memory=self._memory)
+            self._chat_history = [(query, self._answer if self._answer else "")]
+            tool = Tool(
+                name='ConversationalRetrievalChain', 
+                func = lambda query: qa({"question": query, "chat_history": self._chat_history}), 
+                description = 'useful for when you need to answer questions about current files' 
+            )
+            tools = load_tools(["bing-search", "llm-math"], llm=llm)
+            tools.append(tool)
+            memory = ConversationBufferMemory(memory_key="chat_history")
+            agent_chain = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory, handle_parsing_errors=True)
+            self._agent = agent_chain
+            # agent = create_agent(tool, loader)
+
+        self._answer = self._agent.run(query)
         # index = create_index(loader)
         # answer = index.query(query, llm=llm)
         # answer = llm(query)
-        return self._answer["answer"]
+        return self._answer
--- bots/utils/agent.py
+++ bots/utils/agent.py
@@ -86,10 +86,10 @@ from bots.utils.models import llm
 # agent = PlanAndExecute(planner=planner, executor=executor, verbose=True)
 
 ## conversational agent
-def create_agent(loader=None):
+def create_agent(tool, loader=None):
     tools = load_tools(["bing-search", "llm-math"], llm=llm)
+    tools.append(tool)
     memory = ConversationBufferMemory(memory_key="chat_history")
-    index = create_index(loader)
-    agent_chain = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory, index=index, handle_parsing_errors=True)
+    agent_chain = initialize_agent(tools, llm, agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory, handle_parsing_errors=True)
     agent = agent_chain
     return agent
